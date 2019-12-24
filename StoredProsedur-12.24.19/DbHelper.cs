508d34baeabb55a24a571fe3652536a39ab5abe6
rafet <rafet@csharp>
2019-12-24 20:29:13 +0400

    Add project files.

---
+++ StoredProsedur-12.24.19/DbHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace StoredProsedur_12._24._19
+{
+    
+    static class DbHelper
+    {
+        private static string connectionString = "Data Source=DESKTOP-8T1KHOD;Initial Catalog=UniversityDb;Integrated Security=True";
+
+        public static DataTable GetAllStudents()
+        {
+            DataTable dataTable = new DataTable();
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+            SqlCommand command = new SqlCommand("GetAllStudents", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            dataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+        public static int GetStudentCountWithAge(int age)
+        {
+            int count = 0;
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = connectionString;
+            connection.Open();
+            SqlCommand command = new SqlCommand("GetStudentCountWithAge", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("age", age);
+            count =Convert.ToInt32(command.ExecuteScalar());
+            connection.Close();
+            return count;
+        }
+    }
+}
---
+++ StoredProsedur-12.24.19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredProsedur_12._24._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable students = new DataTable();
+            students = DbHelper.GetAllStudents();
+            PrintTable(students);
+            Console.WriteLine(DbHelper.GetStudentCountWithAge(23));
+            Console.ReadKey();
+        }
+
+        static void PrintTable(DataTable students)
+        {
+            foreach (DataColumn column in students.Columns)
+            {
+                Console.Write($"{column,-15} ");
+            }
+            Console.WriteLine();
+            foreach ( DataRow  row in students.Rows)
+            {
+                foreach (DataColumn column in students.Columns)
+                {
+                    Console.Write($"{row[column],-15} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
